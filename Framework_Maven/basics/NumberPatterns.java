32ded144c282ea749c69229ad23f73096562a0c5
kumarswamy830 <kumarswamy830@gmail.com>
2014-12-10 01:32:45 +0530

    yes

--- Framework_Maven/basics/NumberPatterns.java
+++ Framework_Maven/basics/NumberPatterns.java
@@ -5,7 +5,7 @@ public class NumberPatterns {
 	public static void main(String[] args) {
 		
 		int i, j;
-		 for (i = 0; i < 4; i++) {
+		 for (i = 0; i < 5; i++) {
 			 
 			 
 		  for (j = 0; j <= i; j++) {
